9005f2ec128fbe1cf29df51a8497185ccf18042d
Elmer Dema <elmerdema2022@gmail.com>
2023-12-16 18:17:03 +0100

    gui changes+dataset added+main.txt

--- gui.py
+++ gui.py
@@ -9,10 +9,11 @@ ization.
 1
 
 '''
-# dataset 1: https://www.kaggle.com/datasets/jeremylarcher/american-house-prices-and-demographics-of-top-cities/code
-# dataset 2: https://www.kaggle.com/datasets/adampq/air-quality-index-by-state-1980-2022
+# dataset 1: https://www.kaggle.com/datasets/johnsmith88/heart-disease-dataset/
+# might help us: https://www.kaggle.com/code/nareshbhat/heart-attack-prediction-using-different-ml-models/notebook
 
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QTabWidget
+
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QTabWidget, QLabel, QGridLayout, QSlider, QHBoxLayout
 from PyQt6.QtCore import Qt
 import pandas as pd
 import numpy as np
@@ -23,38 +24,100 @@ class Widget(QWidget):
         super().__init__()
         self.setWindowTitle("Recommendation System")
         self.resize(750, 550)
-
-    def buttons(self):
-        self.button1 = QPushButton("Button 1")
-        self.button2 = QPushButton("Button 2")
-        self.button3 = QPushButton("Button 3")
-
-        layout = QVBoxLayout(self)
-        layout.addWidget(self.button1)
-        layout.addWidget(self.button2)
-        layout.addWidget(self.button3)
+        self.tabs_section()        
+        self.tabs.setCurrentIndex(0)
 
     def tabs_section(self):
-        tabs = QTabWidget(self)
-        tabs.setTabPosition(QTabWidget.TabPosition.North)
-        tabs.setMovable(True)
+        self.tabs = QTabWidget(self)
+        self.tabs.setTabPosition(QTabWidget.TabPosition.North)
+        self.tabs.setMovable(True)
 
         # Show all tabs without arrow buttons
-        tabs.setDocumentMode(True)
+        self.tabs.setDocumentMode(True)
 
         tab1 = QWidget()
         tab2 = QWidget()
 
-        tabs.addTab(tab1, "Home")
-        tabs.addTab(tab2, "Visualisation")
-        tabs.setMinimumWidth(200)
+        self.tabs.addTab(tab1, "Home")
+        self.tabs.addTab(tab2, "Visualization")
+
         # Set elide mode for tab text (adjust as needed)
-        tabs.setElideMode(Qt.TextElideMode.ElideRight)
+        self.tabs.setElideMode(Qt.TextElideMode.ElideRight)
+
+        self.tabs.currentChanged.connect(self.tab_changed)
+
+        # Create a layout for the main widget
+        main_layout = QVBoxLayout(self)
+        main_layout.addWidget(self.tabs)
+
+        # Set the layout for the main widget
+        self.setLayout(main_layout)
+
+    def tab_changed(self, index):
+        if index == 0:
+            self.display_home()
+        elif index == 1:
+            self.display_visualization()
+
+    def display_home(self):
+        # Create a widget for the home tab
+        home_tab_widget = QWidget()
+        grid_layout = QGridLayout(home_tab_widget)
+
+        # Create and add buttons to the layout
+        self.label_gender = QLabel("Gender")
+
+        # Create a horizontal layout for the gender buttons
+        button_layout = QHBoxLayout()
+
+        self.button_male = QPushButton("Male")
+        self.button_female = QPushButton("Female")
+
+        # Add buttons to the horizontal layout
+        button_layout.addWidget(self.button_male)
+        button_layout.addWidget(self.button_female)
+
+        # Create and add other widgets to the layout
+        self.label_age = QLabel("Age")
+        self.slider_age = QSlider(Qt.Orientation.Horizontal)
+        self.slider_age.setMaximumWidth(150)
+        self.slider_age.setMinimum(29)
+        self.slider_age.setMaximum(77)
+        self.slider_age.setValue(53)
+        self.slider_age.setTickInterval(1)
+        self.slider_age.valueChanged.connect(self.value_changed)
+        self.label_slider_age = QLabel("Age: " + str(self.slider_age.value()))
+
+        # Add widgets to the grid layout
+        grid_layout.addWidget(self.label_gender, 0, 0)
+        grid_layout.addLayout(button_layout, 1, 0)  # Add the button layout to the same row
+        grid_layout.addWidget(self.label_age, 2, 0)
+        grid_layout.addWidget(self.slider_age, 3, 0)
+        grid_layout.addWidget(self.label_slider_age, 3, 1)
+
+
+        # Set the layout for the home tab
+        home_tab_widget.setLayout(grid_layout)
+
+        # Check if a layout already exists for the home tab
+        if self.tabs.widget(0).layout():
+            # Clear the existing layout for the home tab
+            for i in reversed(range(self.tabs.widget(0).layout().count())):
+                self.tabs.widget(0).layout().itemAt(i).widget().setParent(None)
+
+        # Set the layout for the current tab
+        self.tabs.widget(0).setLayout(QVBoxLayout(self.tabs.widget(0)))
+        self.tabs.widget(0).layout().addWidget(home_tab_widget)
+
+
+    def display_visualization(self):
+        pass
+
+    def value_changed(self):
+        self.label_slider_age.setText("Age: " + str(self.slider_age.value()))
 
 if __name__ == "__main__":
     app = QApplication([])
     widget = Widget()
-    widget.buttons()
-    widget.tabs_section()
     widget.show()
     app.exec()
